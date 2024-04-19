a77ffce3591d2eb2e7d6a4f4552c832e1fa82ea4
PC Thiago <thiago.hashimoto94@gmail.com>
2024-04-18 22:47:31 -0300

    Developing navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './common/header/header.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: HomeComponent},
+    {path:'login', component: LoginComponent}
+];
